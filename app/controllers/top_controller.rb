143897e34bffcb512b9643a995ed7beb301904c5
RK3833 <wkc1wnr2@gmail.com>
2016-10-12 01:14:22 +0000

    scraiping

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,27 @@
+class TopController < ApplicationController
+	require 'open-uri'
+	require 'nokogiri'
+
+	def index
+		url = 'http://matome.naver.jp/tech'
+
+		charset = nil
+		html = open(url) do |f|
+  			charset = f.charset # 文字種別を取得
+  			f.read # htmlを読み込んで変数htmlに渡す
+		end
+
+		doc = Nokogiri::HTML.parse(html, nil, charset)
+
+		doc.xpath('//li[@class="mdTopMTMList01Item"]').each do |node|
+		  # tilte
+		  p node.css('h3').inner_text
+
+		  # 記事のサムネイル画像
+		  p node.css('img').attribute('src').value
+
+		  # 記事のサムネイル画像
+		  p node.css('a').attribute('href').value
+		end
+	end
+end
